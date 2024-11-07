b786e575974ef35b51c068d499aad923bc67dac1
Thibaud Meynier <thibaud.meynier@laposte.net>
2024-11-07 16:39:17 +0100

    Red partiel M1000 et Bergame

--- Scrapping tennis data/Tournament/V_TOURNAMENT_F.R
+++ Scrapping tennis data/Tournament/V_TOURNAMENT_F.R
@@ -29,7 +29,7 @@ library(tidyverse)
 #Santiago(SF) Kitzbuel (QF)
 V_1000=V_TOURNAMENT4 %>% filter(Categorie %in% c("ATP 1000"))
 
-V_TOURNAMENT_RED = V_1000 %>% filter(N >=95)
+V_TOURNAMENT_RED = V_1000 %>% filter(N <95)
 
 # V_TOURNAMENT_RED2 = V_1000 %>% 
 # filter(N <95) %>% 
@@ -37,7 +37,7 @@ V_TOURNAMENT_RED = V_1000 %>% filter(N >=95)
 # unique()
 
 V_TOURNAMENT_RED2 = V_1000 %>% 
-  filter(N >=95) %>%
+  filter(N <95) %>%
   select(tournament,Year,Week_tournament, Round, Ranking_points) %>%
   unique()
 
@@ -47,15 +47,15 @@ n=3
 new_rows <- V_TOURNAMENT_RED2 %>%
   distinct(tournament,Year,Week_tournament,Round) %>%              # Obtenir les tournois uniques
   slice(rep(1:n(), each = n)) %>%        # Répéter chaque tournoi 3 fois
-  mutate(Round = rep(c("Q-1R","Q-2R","Q-R2RW"), times = n()/n),
-         Ranking_points = rep(c(0,8,16), times = n()/n)) %>% 
+  mutate(Round = rep(c("Q-1R","Q-R16","Q-QW"), times = n()/n),
+         Ranking_points = rep(c(0,8,25), times = n()/n)) %>% 
   distinct()
 
 # Combiner avec les lignes existantes
 V_TOURNAMENT_RED2 = rbind(new_rows, V_TOURNAMENT_RED2)
 
 # Effectuer la jointure gauche avec les autres colonnes du dataset initial
-V_TOURNAMENT_RED_P2 = V_TOURNAMENT_RED %>%
+V_TOURNAMENT_RED_P1 = V_TOURNAMENT_RED %>%
   select(tournament, Date, Categorie, Country_tournament, Surface_tournament, Week_tournament, Year) %>%
   left_join(V_TOURNAMENT_RED2, by = c("tournament","Year","Week_tournament")) %>%
   unique()
@@ -88,9 +88,15 @@ load("C:/Users/Thiti/Desktop/Tennis-Data//Scrapping tennis data/Tournament/RED/V
 
 V_TOURNAMENT_F=rbind(V_50,V_75_80,V_90_125,V_175,V_250,V_500,V_1000,V_2000)
 
+# V_TOURNAMENT_F=V_TOURNAMENT_F %>% 
+#   mutate(Round=case_when(Categorie=="ATP 1000" & Round=="Q-R16W"~"Q-QW",
+#                          Categorie=="ATP 1000" & Round=="Q-2RW"~"Q-QW",
+#                          TRUE~Round
+#   ))
+
 save(V_TOURNAMENT_F,file = paste0(getwd(),"/Scrapping tennis data/Tournament/V_TOURNAMENT_F.RData"))
 
-# RED V_TOURNAMENT_F
+##### RED V_TOURNAMENT_F #####
 
 TOURNAMENT_RED=V_TOURNAMENT_F %>% filter(tournament=="Bergamo Challenger" & Year==2021)
 
@@ -102,7 +108,7 @@ new_rows=data.frame(
   Surface_tournament = TOURNAMENT_RED$Surface_tournament[1],
   Week_tournament = 8,
   Year = 2020,
-  Round = c("Q-R16","Q-QF","Q-QFW","1R","2R","R16","QF","SF","F","Winner"),              # Valeur manquante pour Round
+  Round = c("Q-R16","Q-QF","Q-QW","1R","2R","R16","QF","SF","F","Winner"),              # Valeur manquante pour Round
   Ranking_points = c(0,2,4,0,4,7,15,29,48,80)      # Valeur manquante pour Ranking_points
 )
 
@@ -113,10 +119,6 @@ V_TOURNAMENT_F=V_TOURNAMENT_F %>% filter(!(tournament=="Bergamo Challenger" & Ye
 
 V_TOURNAMENT_F=rbind(V_TOURNAMENT_F,new_rows)
 
-V_TOURNAMENT_F=V_TOURNAMENT_F %>% 
-  mutate(Round=case_when(Categorie=="ATP 1000" & Round=="Q-R16W"~"Q-QW",
-                         Categorie=="ATP 1000" & Round=="Q-2RW"~"Q-QW",
-                         TRUE~Round
-                         ))
-
 save(V_TOURNAMENT_F,file = paste0(getwd(),"/Scrapping tennis data/Tournament/V_TOURNAMENT_F.RData"))
+
+# Faire les modifs pour ajout info table paramètres
