2a35aa947ec71c7b2076f2f622d02168649de537
Thibaud-Meynier <72493328+Thibaud-Meynier@users.noreply.github.com>
2025-07-22 16:16:37 +0200

    Update Elo rating.R
    modif du calcul du elo rate

--- Scrapping tennis data/Elo rating.R
+++ Scrapping tennis data/Elo rating.R
@@ -8,9 +8,10 @@
 
 # l'issue du match
 
-i=2890
+#i=1
 
 tournament=V_MATCH_t %>% 
+  filter(Phase=="Main Draw") %>% 
   arrange(Date,tournament,desc(N_match),Season)
 
 tournament$Elo_W=NA
@@ -21,7 +22,7 @@ tournament$Elo_W_NEW=NA
 
 tournament$Elo_L_NEW=NA
 
-for (i in 10001:13544){
+for (i in 110001:nrow(tournament)){
   
   # on récup pour 1 match donné les infos de chaque joueur et du match
   
@@ -37,6 +38,8 @@ for (i in 10001:13544){
   
   Round=row$Round
   
+  info=row$info
+  
   # on regarde l'historique du joueur p1
   hist_p1=tournament %>% filter((Winner_id==p1|Loser_id==p1) & (Date<=date_match))
   
@@ -50,7 +53,7 @@ for (i in 10001:13544){
   
   last_match=tail(hist_p1,1)
   
-  hist_p1=hist_p1 %>% filter(N_match==last_match$N_match & tournament==last_match$tournament)
+  #hist_p1=hist_p1 %>% filter(N_match==last_match$N_match & tournament==last_match$tournament)
   
   # Calcul de la durée entre le match et leur dernier match
   
@@ -73,7 +76,7 @@ for (i in 10001:13544){
   
   last_match=tail(hist_p2,1)
   
-  hist_p2=hist_p2 %>% filter(N_match==last_match$N_match & tournament==last_match$tournament)
+  #hist_p2=hist_p2 %>% filter(N_match==last_match$N_match & tournament==last_match$tournament)
   
   # Calcul de la durée entre le match et leur dernier match
   
@@ -85,21 +88,38 @@ for (i in 10001:13544){
   
   # on recherche si le joueur 1 a déjà disputé un match ou non
   
-  count_match_p1=nrow(tournament %>% filter((Winner_id==p1|Loser_id==p1) & (Date<=date_match & N_match!=n_match)))
+  threshold=300
   
-  count_match_p2=nrow(tournament %>% filter((Winner_id==p2|Loser_id==p2) & (Date<=date_match & N_match!=n_match)))
+  count_match_p1=hist_p1 %>% nrow()
   
-  # calcul dummy sur penalité absence joueur 
+  count_match_p1=ifelse(count_match_p1>=threshold,threshold,count_match_p1)
   
-  penalty_p1=ifelse(between(dday_p1,60,80),1,
-         ifelse(between(dday_p1,81,180),1.25,
-                ifelse(dday_p1>180,1.5,0)))
+  count_match_p2=hist_p2 %>% nrow()
   
-  penalty_p2=ifelse(between(dday_p2,60,80),1,
-                    ifelse(between(dday_p2,81,180),1.25,
-                           ifelse(dday_p2>180,1.5,0)))
+  count_match_p2=ifelse(count_match_p1>=threshold,threshold,count_match_p2)
   
- 
+  # calcul des dummy pour le k factor
+  
+  Round_adjust <- ifelse(Round == "F", 1,
+                         ifelse(Round == "SF", 0.9,
+                                ifelse(Round == "QF", 0.8, 0.7)))
+  
+  Walkover=ifelse(row$info=="Completed",1,0.5)
+  
+  penalty=function(diff_date){
+    
+    penalty=ifelse(between(diff_date,60,80),0.7,
+                 ifelse(between(diff_date,81,180),0.85,
+                        ifelse(diff_date>180,1,0)))
+    
+    return(penalty)
+  }
+  
+  covid=ifelse(between(date_match,as.Date("2020-08-01"),as.Date("2021-02-01")) ,0,1)
+
+  penalty_p1=penalty(dday_p1)*covid
+
+  penalty_p2=penalty(dday_p2)*covid
   
   # on reprend le dernier elo (1500 si pas d'historique)
   
@@ -110,20 +130,38 @@ for (i in 10001:13544){
   proba_p1 <- 1 / (1 + 10 ^ ((elo_p2 - elo_p1)/400)) # calcul des probas données par le Elo
   proba_p2 <- 1 / (1 + 10 ^ ((elo_p1 - elo_p2)/400)) 
   
-  k_p1=250/((count_match_p1+5)^0.4) # calcul du facteur k pour chaque joueur
-  k_p2=250/((count_match_p2+5)^0.4)
+  rating=function(elo_p){
+    
+    rating=1+ 18/(1+(2^((elo_p-1500)/100)))
+  
+    return(rating)
+  }
+  
+  # Prime de GC
+  
+  level=ifelse(row$Categorie=="Grand Slam",1,
+               ifelse(row$Categorie %in% c("Olympics","Masters"),0.95,
+                    ifelse(row$Categorie=="ATP 1000",0.9,
+                      ifelse(row$Categorie %in% c("ATP 500","Team"),0.8,
+                             ifelse(row$Categorie=="ATP 250",0.7,0.65)
+                             )
+                      )
+                    )
+               )
+  
+  # k_p1=250/((count_match_p1+5)^0.4)*level*Round_adjust*Walkover*rating(elo_p1) # calcul du facteur k pour chaque joueur
+  # k_p2=250/((count_match_p2+5)^0.4)*level*Round_adjust*Walkover*rating(elo_p2)
+  
+   k_p1=32*level*Round_adjust*Walkover*rating(elo_p1)
+   k_p2=32*level*Round_adjust*Walkover*rating(elo_p2)
   
   # dummy pour calculer le nouveau elo
   s_p1=1 
   s_p2=0
   
-  # Prime de GC
+  elo_p1_new=elo_p1+(k_p1)*(s_p1-proba_p1)
   
-  level=ifelse(row$Categorie=="Grand Slam",1.1,1)
-  
-  elo_p1_new=elo_p1+(k_p1*level)*(s_p1-proba_p1)
-  
-  elo_p2_new=elo_p2+(k_p2*level)*(s_p2-proba_p2)
+  elo_p2_new=elo_p2+(k_p2)*(s_p2-proba_p2)
   
   # tournament$Elo_W_prec[i]=round(elo_p1,2)
   # tournament$Elo_L_prec[i]=round(elo_p2,2)
