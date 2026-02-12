48ae4de14753d934ed08183c8186ba0e9d28bec9
Thibaud-Meynier <72493328+Thibaud-Meynier@users.noreply.github.com>
2026-02-12 08:44:57 +0100

    Update prepa_bet_ml.R
    modification de la jointure avec la table de classement

--- Scrapping tennis data/ML_engenering/prepa_bet_ml.R
+++ Scrapping tennis data/ML_engenering/prepa_bet_ml.R
@@ -1,3 +1,245 @@
+library(here)
+library(tidyverse)
+library(data.table)
+library(progress)
+library(rvest)
+library(dplyr)
+library(stringr)
+library(lubridate)
+library(xml2)
+library(sjmisc)
+
+source(paste0(getwd(),"/Scrapping tennis data/Stat function.R"))
+
+load(paste0(getwd(),"/Scrapping tennis data/Info_players/V_PLAYERS_RED.RData"))
+
+year_lim=2017
+
+V_PLAYERS=V_PLAYERS %>% 
+  group_by(Player_name,Birth_date,Hand,Size,Weight) %>% 
+  mutate(CLE_JOUEUR=row_number()) %>% 
+  filter(CLE_JOUEUR==1) %>% 
+  select(-CLE_JOUEUR)
+
+load(paste0(getwd(),"/Scrapping tennis data/Extraction/V_MATCH_2009_2016.RData"))
+
+V_MATCH_2009_2016=V_MATCH
+
+V_MATCH_2009_2016=V_MATCH_2009_2016 %>% 
+  rename(Winner_id=P1,
+         Loser_id=P2)
+
+colnames=colnames(V_MATCH_2009_2016)
+
+rm(V_MATCH)
+rm(V_MATCH_2009_2016)
+
+V_MATCH=data.frame()
+
+#i=2016
+
+for (i in year_lim:2025){
+  
+  load(file = paste0(getwd(),"/Scrapping tennis data/Extraction/ATP_",i,"_Extraction.RData"))
+  
+  table_stock$Season=i
+  
+  if ("P1" %in% names(table_stock)==T){
+    
+    table_stock=table_stock %>% 
+      rename(Winner_id=P1,
+             Loser_id=P2)
+  }else{
+    
+    table_stock=table_stock=table_stock %>% select(colnames)
+  }
+  
+  V_MATCH=rbind(V_MATCH,table_stock)
+  
+  print(i)  
+  
+}
+
+rm(table_stock)
+
+
+load(paste0(getwd(),"/Scrapping tennis data/Tournament/V_TOURNAMENT_F_2017_2023.RData"))
+
+V_TOURNAMENT_2017_2023=V_TOURNAMENT_F %>% filter(Year>=year_lim)
+
+load(paste0(getwd(),"/Scrapping tennis data/Tournament/V_TOURNAMENT_F_2024.RData"))
+
+V_TOURNAMENT_2024=V_TOURNAMENT_F
+
+load(paste0(getwd(),"/Scrapping tennis data/Tournament/V_TOURNAMENT_F_2025.RData"))
+
+V_TOURNAMENT_2025=V_TOURNAMENT_F
+
+V_TOURNAMENT_F=rbind(V_TOURNAMENT_2017_2023,V_TOURNAMENT_2024,V_TOURNAMENT_2025)
+
+rm(V_TOURNAMENT_2017_2023,V_TOURNAMENT_2024,V_TOURNAMENT_2025)
+
+
+V_TOURNAMENT_INFO=V_TOURNAMENT_F %>% 
+  select(tournament,Categorie,Country_tournament,Week_tournament,Year,Surface_tournament) %>% 
+  unique() %>% 
+  mutate(Categorie=case_when(Categorie=="ATP 2000"~"Grand Slam",
+                             TRUE~Categorie)) %>% 
+  mutate(CLE_TOURNAMENT=toupper(tournament))
+
+rm(V_TOURNAMENT_F)
+
+
+V_MATCH_t=V_MATCH %>% 
+  mutate(CLE_TOURNAMENT=toupper(tournament)) %>% 
+  left_join(V_TOURNAMENT_INFO %>% select(-tournament),by=c("CLE_TOURNAMENT","Season"="Year")) %>% 
+  select(-CLE_TOURNAMENT) %>% 
+  filter(!tournament %in% c("Riyadh - Exhibition","Next Gen Atp Finals")) %>% 
+  mutate(Categorie=case_when(tournament %in% c("United Cup","Atp Cup","Davis Cup")~"Team",
+                             tournament=="Masters Cup Atp"~"Masters",
+                             tournament %like% "Olympics"~"Olympics",
+                             tournament=="Reunion Challenger" & Season==2011~"Challenger 80",
+                             TRUE~Categorie)) %>% 
+  mutate(Surface_tournament=case_when(is.na(Surface_tournament)~Surface,
+                                      Categorie=="Masters" & Season %in% c(2005,2006,2007,2008)~"Indoors",
+                                      tournament=="Madrid" & Season %in% c(2003:2008)~"Indoors",
+                                      tournament=="Bangkok" & Season %in% c(2003:2004)~"Indoors",
+                                      TRUE~Surface_tournament)) %>% 
+  mutate(Surface_tournament=case_when(Surface_tournament=="clay"~"Clay",
+                                      Surface_tournament=="hard"~"Hard",
+                                      Surface_tournament=="grass"~"Grass",
+                                      Surface_tournament=="indoors"~"Indoors",
+                                      TRUE~Surface_tournament)) %>% 
+  mutate(Week_tournament=isoweek(Date),
+         Country_tournament=case_when(tournament=="Olympics - Paris"~"France",
+                                      tournament=="Olympics - Tokyo"~"Japan",
+                                      tournament=="Masters Cup Atp" & Season>=2021~"Italy",
+                                      tournament=="Masters Cup Atp" & Season %in% c(2009:2020)~"Great Britain",
+                                      TRUE~Country_tournament)) 
+
+V_MATCH_t=V_MATCH_t %>% 
+  group_by(tournament,Country_tournament,Season,Phase,Round,Date,Week_tournament,Winner_id,Loser_id) %>% 
+  mutate(CLE_LIGNE=row_number()) %>% 
+  filter(CLE_LIGNE==1) %>% 
+  select(-CLE_LIGNE)
+
+#V_MATCH_t %>% group_by(Categorie) %>% count()
+
+# "Challenger 125","Challenger 175",
+# "Challenger 110","Challenger 100",
+# "Challenger 90","Challenger 80",
+# "Challenger 75","Challenger 50"
+
+V_MATCH_HIST=V_MATCH_t
+
+V_MATCH_t=V_MATCH_t %>% 
+  filter(Phase=="Main Draw" & 
+           (Categorie %in% c("Grand Slam","Olympics","Masters",
+                             "ATP 1000","ATP 500",'ATP 250')|tournament %in% c("Atp Cup","United Cup")) & Season>=year_lim)
+
+V_MATCH_t=V_MATCH_t %>% ungroup() %>% 
+  select(tournament,Country_tournament,Season,Date,Week_tournament,Categorie,Surface_tournament,Round,Winner_id,Loser_id,Odd_W,Odd_L,info,Winner,Loser)
+
+
+get_tennis_week <- function(date) {
+  year <- year(date)
+  week_num <- isoweek(date)
+  
+  # Gérer le cas des premiers jours de janvier qui appartiennent 
+  # à la dernière semaine de l'année précédente
+  if (month(date) == 1 && week_num >= 52) {
+    year <- year - 1
+  }
+  
+  # Gérer le cas de fin décembre appartenant à la semaine 1 de l'année suivante
+  if (month(date) == 12 && week_num == 1) {
+    year <- year + 1
+  }
+  
+  return(paste0(year, "-", sprintf("%02d", week_num)))
+}
+
+V_RANK=data.frame()
+
+for (i in year_lim:2025){
+  
+  load(file = paste0(getwd(),"/Scrapping tennis data/Rank/RANK_ATP_",i,".RData"))
+  
+  V_RANK=rbind(V_RANK,rank)
+  
+  print(i)  
+  
+}
+
+V_RANK=V_RANK %>% 
+  select(-Move) %>% 
+  rename("Player_name"="Player name") %>% 
+  mutate(Week_Rank = sapply(Date, get_tennis_week))
+
+
+rm(rank)
+rm(V_TOURNAMENT_INFO)
+rm(V_MATCH)
+
+V_MATCH_HIST=V_MATCH_HIST %>% 
+  mutate(Match_week=sapply(Date, get_tennis_week)) %>% 
+  left_join(V_RANK %>% select(Rank,Player_name,Points,Week_Rank) %>% 
+              rename(Rank_W=Rank,Points_W=Points) %>% 
+              mutate(Rank_W=as.numeric(Rank_W),
+                     Points_W=as.numeric(Points_W)),
+            by=c("Winner_id"="Player_name","Match_week"="Week_Rank")) %>% 
+  left_join(V_RANK %>% select(Rank,Player_name,Points,Week_Rank) %>% 
+              rename(Rank_L=Rank,Points_L=Points) %>% 
+              mutate(Rank_L=as.numeric(Rank_L),
+                     Points_L=as.numeric(Points_L)),
+            by=c("Loser_id"="Player_name","Match_week"="Week_Rank")) %>% 
+  mutate(Rank_W=case_when(is.na(Rank_W)~1000,TRUE~Rank_W),
+         Points_W=case_when(is.na(Points_W)~10,TRUE~Points_W),
+         Rank_L=case_when(is.na(Rank_L)~1000,TRUE~Rank_L),
+         Points_L=case_when(is.na(Points_W)~10,TRUE~Points_L))
+
+V_MATCH_t=V_MATCH_t %>% 
+  mutate(Match_week=sapply(Date, get_tennis_week)) %>% 
+  left_join(V_RANK %>% select(Rank,Player_name,Points,Week_Rank) %>% 
+              rename(Rank_W=Rank,Points_W=Points) %>% 
+              mutate(Rank_W=as.numeric(Rank_W),
+                     Points_W=as.numeric(Points_W)),
+            by=c("Winner_id"="Player_name","Match_week"="Week_Rank")) %>% 
+  left_join(V_RANK %>% select(Rank,Player_name,Points,Week_Rank) %>% 
+              rename(Rank_L=Rank,Points_L=Points) %>% 
+              mutate(Rank_L=as.numeric(Rank_L),
+                     Points_L=as.numeric(Points_L)),
+            by=c("Loser_id"="Player_name","Match_week"="Week_Rank")) %>% 
+  mutate(Rank_W=case_when(is.na(Rank_W)~1000,TRUE~Rank_W),
+         Points_W=case_when(is.na(Points_W)~10,TRUE~Points_W),
+         Rank_L=case_when(is.na(Rank_L)~1000,TRUE~Rank_L),
+         Points_L=case_when(is.na(Points_W)~10,TRUE~Points_L))
+
+rm(V_RANK)
+
+## Ajout info joueurs 
+
+V_MATCH_t=V_MATCH_t %>% 
+  left_join(V_PLAYERS %>% select(-Best_Rank) %>% 
+              rename(Country_W=Country,
+                     URL_W=URL_players,
+                     Size_W=Size,
+                     Weight_W=Weight,
+                     Birth_date_W=Birth_date,
+                     Hand_W=Hand),
+            by=c("Winner_id"="Player_name")) %>% 
+  left_join(V_PLAYERS %>% select(-Best_Rank) %>% 
+              rename(Country_L=Country,
+                     URL_L=URL_players,
+                     Size_L=Size,
+                     Weight_L=Weight,
+                     Birth_date_L=Birth_date,
+                     Hand_L=Hand),
+            by=c("Loser_id"="Player_name"))
+
+V_MATCH_HIST=as.data.table(V_MATCH_HIST)
+
+##### Stats players #####
 
 # Global
 
