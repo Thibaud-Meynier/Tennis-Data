c418d98f333c10d2bebb9d8479face928f49f3f6
Thibaud-Meynier <72493328+Thibaud-Meynier@users.noreply.github.com>
2026-03-09 21:59:53 +0100

    Update Value_analysis.R
    ajout d'une boucle pour analyser tous les modèles en même temps

--- Scrapping tennis data/ML_engenering/Value_analysis.R
+++ Scrapping tennis data/ML_engenering/Value_analysis.R
@@ -1,3 +1,4 @@
+library(glue)
 
 is_value=function(pred,margin=0,U=1){
     
@@ -43,46 +44,125 @@ is_value=function(pred,margin=0,U=1){
   
 }
 
+model_list=c("KNN","Naive_Bayes","NN1","Random_Forest","Ridge","LASSO","Elastic","LDA","XGB","LGB","GBM","P_F_comb","Ensemble_Pred")
 
-Fav_1=is_value("KNN",margin=0) %>% 
-  select(-c(P_F_pred,P_F_market,Ratio_Odd_O,Ratio_Odd_F,Signal)) %>% 
-  filter(!is.na(Value)) %>% 
-  filter(Value=="Value Fav1") %>% 
-  mutate(
-    Ratio_Bin = cut(
-      Ratio_Value,
-      breaks = c(0, 1.05, 1.10, 1.25, Inf),
-      labels = c("Faible (0-5%)","Moyen (5-10%)", "Intéréssant (10-25%)", "Fort (25%+)"),
-      right          = FALSE,
-      include.lowest = TRUE
-    ),
-    
-    Odd_Bin = cut(
-      Odd_played,
-      breaks = c(1,1.10, 1.25, 1.35, 1.55, 1.70, 1.85, Inf),
-      labels = c("1-1.10","1.10-1.25", "1.25-1.35", "1.35-1.55",
-                 "1.55-1.70", "1.70-1.85", "1.85+"),
-      right          = FALSE,
-      include.lowest = TRUE
-    )) %>% 
-  group_by(Categorie, Ratio_Bin) %>%
-  mutate(
-    n_bet=row_number(),
-    
-   BK = cumsum(Result)) 
+value=data.frame()
+
+for (i in model_list){
+  
+  temp=is_value(i,margin=5) %>% 
+    select(-c(P_F_pred,P_F_market,Ratio_Odd_O,Ratio_Odd_F,Signal)) %>% 
+    filter(!is.na(Value)) %>% 
+    filter(between(Odd_played,1.35,4.5)) %>% 
+    mutate(
+      Ratio_Bin = cut(
+        Ratio_Value,
+        breaks = c(0, 1.05, 1.10, 1.25, Inf),
+        labels = c("Faible (0-5%)","Moyen (5-10%)", "Intéréssant (10-25%)", "Fort (25%+)"),
+        right          = FALSE,
+        include.lowest = TRUE
+      ),
+      
+      Odd_Bin = cut(
+        Odd_played,
+        breaks = c(1, 1.10, 1.25, 1.35, 1.55, 1.70, 1.85, 2, 2.15, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 4, 4.5, 5, Inf),
+        labels = c("1-1.10", "1.10-1.25", "1.25-1.35", "1.35-1.55",
+                   "1.55-1.70", "1.70-1.85", "1.85-2", "2-2.15",
+                   "2.15-2.25", "2.25-2.50", "2.50-2.75", "2.75-3",
+                   "3-3.25", "3.25-3.50", "3.50-4", "4-4.50",
+                   "4.50-5", "5+"),
+        right          = FALSE,
+        include.lowest = TRUE
+      )) %>% 
+    group_by(Value,Ratio_Bin) %>% 
+    mutate(
+      n_bet=row_number(),
+      
+      BK = cumsum(Result),
+      
+      Pred = i) 
+  
+  value=rbind(value,temp)
+  
+  print(i)
+  
+}
 
 
-
-ggplot(Fav_1,aes(x = n_bet, y = BK, group = Ratio_Bin, color = Ratio_Bin)) +
+p=ggplot(value %>% 
+           filter(Value=="Value Fav1"),aes(x = n_bet, y = BK, group = Pred, color = Pred)) +
   geom_line(linewidth = 0.8) +
   geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
-  facet_wrap(~ Categorie, scales = "free") +       # "free" = axes adaptés par catégorie
+  facet_wrap(~ Ratio_Bin, scales = "free") +       # "free" = axes adaptés par catégorie
   scale_color_viridis_d(option = "turbo") +         # rouge→vert naturel pour les tranches de value
   labs(
-    title  = "Bankroll cumulée par catégorie et tranche de ratio de value",
+    title  = glue("Bankroll cumulée par catégorie et tranche de ratio de value"),
     x      = "Nombre de paris",
     y      = "Bankroll cumulée (u.)",
     color  = "Ratio Value"
   ) +
   theme_classic() +
   theme(legend.position = "bottom")
+
+ggplotly(p)
+
+
+
+##### ANALYSE DES DIVERGENCES PAR MODELES #####
+
+divergence <- data.frame()
+
+for (i in model_list){
+  
+  print(i)
+  
+  diverge_model <- test_pred %>% 
+    filter((P_F_market > 0.5 & get(i) <= 0.5) | (P_F_market <= 0.5 & get(i) > 0.5)) %>% 
+    select(tournament, Surface_tournament, Categorie, Season, Round, Favori, Outsider,
+           Odd_F, Odd_O, Issue, all_of(i),Vote_F) %>%   # ← fix ici
+    rename(Prob_Pred = all_of(i)) %>%  
+    mutate(
+      Odd_Played = ifelse(Prob_Pred >= 0.5, Odd_F, Odd_O),
+      Result     = ifelse(
+        (Prob_Pred >= 0.5 & Issue == "Fav_W") | (Prob_Pred < 0.5 & Issue == "Out_W"),
+        Odd_Played - 1,
+        -1
+      ), 
+      Odd_Played_Reverse = ifelse(Prob_Pred >= 0.5, Odd_O, Odd_F),
+      Result_Reverse     = ifelse(
+        (Prob_Pred >= 0.5 & Issue == "Out_W") | (Prob_Pred < 0.5 & Issue == "Fav_W"),
+        Odd_Played_Reverse - 1,
+        -1
+      ),
+      
+      n_bet      = row_number(),
+      Pred       = i
+    )
+  
+  divergence <- rbind(divergence, diverge_model)
+
+  }
+
+divergence <- divergence %>%
+  group_by(Pred) %>%
+  mutate(
+    n_bet  = row_number(),        # Recrée n_bet par modèle
+    BK     = cumsum(Result),
+    BK_Reverse     = cumsum(Result_Reverse)
+  ) %>%
+  ungroup()
+
+p <- ggplot(divergence, aes(x = n_bet, y = BK, group = Pred, color = Pred)) +
+  geom_line()+ 
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
+  theme_classic()
+
+ggplotly(p)
+
+
+divergence %>% 
+  filter(Pred=="NN1") %>% 
+  group_by(Issue,ifelse(Prob_Pred>=0.5,"Fav_W_Pred","Out_W_Pred")) %>% 
+  summarise(N=n())
+
+
